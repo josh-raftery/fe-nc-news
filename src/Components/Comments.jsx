b3c1aae065071ccd943128449f98b8773dcab28a
= <joshuaraftery99@gmail.com>
2024-08-06 11:50:28 +0100

    Task 8 Post comment feature, conditionally coloured text and button colours, error text in placeholder for empty comment submission, error label for error post. Added user context, comments now displayed top to bottom so you see the one posted first

--- src/Components/Comments.jsx
+++ src/Components/Comments.jsx
@@ -1,12 +1,42 @@
-import { useEffect, useState } from "react";
-import { getComments } from "../api";
+import { useContext, useEffect, useState } from "react";
+import { getComments, postComment } from "../api";
 import Loading from "./Loading";
+import { UserContext } from "../contexts/User";
 
 function Comments({ article_id }) {
-  const [commentInput, setCommentInput] = useState(""); //for post later//
+
+  const [badComment,setBadComment] = useState(false)
+  const [badPost, setBadPost] = useState(false)
   const [comment, setComment] = useState("");
+  const [success, setSuccess] = useState(true)
   const [comments, setComments] = useState([]);
   const [loading, setLoading] = useState(true)
+  const { user } = useContext(UserContext)
+  const [userData] = user
+
+  function handleChange(event){
+    setComment(event.target.value)
+  }
+
+  console.log(comments)
+
+  function handleSumbit(event){
+    event.preventDefault()
+    if(!comment){
+      setBadComment(true)
+    } else{
+      setBadComment(false)
+      return postComment(article_id,userData.username,comment)
+      .then(() => {
+        setBadPost(false)
+        setComment("")
+        setSuccess(true) //trigger useEffect - feedback appreicated
+      })
+      .catch((err) => {
+        setBadPost(true)
+      })
+    }
+  }
 
   useEffect(() => {
     getComments(article_id)
@@ -17,7 +47,7 @@ function Comments({ article_id }) {
     .catch((err) => {
         setLoading(false)
     })
-  }, []);
+  }, [success]);
 
   if(loading){
     return (
@@ -27,6 +57,21 @@ function Comments({ article_id }) {
 
   return (
     <>
+      <div className="post-comment">
+      <label class="form-control w-full max-w-xs">
+          <input
+            value={comment}
+            type="text"
+            placeholder={badComment ? "Comment empty" :"Type here"}
+            className={badPost || badComment ? "input input-bordered input-error w-full max-w-xs" : "input input-bordered input-primary w-full max-w-xs"} 
+            onChange={handleChange}
+          />
+          <div className="label">
+            <span className="label-text-alt">{badPost ? "Poor connection, try again later" : ""}</span>
+          </div>
+        </label>
+        <button onClick={handleSumbit} style={{marginLeft: "1rem"}} className={badPost || badComment ? "input input-bordered input-error" : "btn btn-outline btn-primary"}>Post</button>
+      </div>
         {comments.map((currentComment) => {
             return (
                 <div key={currentComment.comment_id} className="card bg-base-100  shadow-xl">
@@ -39,9 +84,13 @@ function Comments({ article_id }) {
                 </div>
                 </div>
             )
-        })}
+        }).reverse()}
     </>
   );
 }
+
+//post comment
+//need author - user
+//aericle_id
 
 export default Comments;
